534a8f0d328480fb836c80ff63e4e2a1c7d7ef03
Denispro567 <Denis567@i.ua>
2020-10-12 10:31:59 +0300

    Moduled version

--- add_all.sh
+++ add_all.sh
@@ -1,12 +1,27 @@
-if [ $# = 0 ]
+function scope() {
+	echo "${args[@]}"
+	echo "Numbers: $#"
+	echo "They are: $*"
+}
+
+function empty() {
+	return `[ ${#args[@]} = 0 ]`
+}
+
+# Verification
+
+args=("$@")
+if empty
 then
 	echo "You should enter number-list"
 	echo "Example: ./add_all.sh 1 2 5"
 	exit 1
 fi
 
+# Main logic
+
 let sum=0
-for number #in $*
+for number in "${args[@]}" 
 do
 	let sum+=number
 done
